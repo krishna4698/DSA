a8ac57f88bbca10a8d42a7ea287dc0d1eaab318a
krishna joshi <krishnajoshi4698@gmail.com>
2025-08-25 11:53:19 +0530

    binary search trees

---
+++ BST/bst.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+#include<vector>
+
+
+class node{
+    public:
+    int data;
+    node* left;
+    node* right;
+    node(int  val){
+        data=val;
+        left=right=NULL;
+
+    }
+};
+
+node*insert(node*root,int val){
+if(root==NULL) {
+    return new node(val);
+}
+if(val< root->data){
+     root->left= insert(root->left,val);
+}
+else{
+    root->right=insert(root->right,val);
+}
+return root;
+}
+
+node*Buildbst(vector<int>arr){
+    node*root=NULL;
+    for(int val:arr){
+      root=  insert(root,val);
+    }
+    return root;
+}
+void inorder(node*root){
+    if(root==NULL) return;
+    inorder(root->left);
+    cout<<root->data;
+    inorder(root->right);  
+
+}
+
+int main(){
+vector<int>arr={ 3,2,1,5,6,4};
+node*root=Buildbst(arr);
+inorder(root);
+cout<<endl;
+}
