d5303ee3a7f4ac2fdaa2ef9f223d9be5c7bcd716
krishna joshi <krishnajoshi4698@gmail.com>
2025-10-09 15:45:14 +0530

    maxconsecutive3

---
+++ slidingwindow/maxconsecutive3.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+ 
+int main(){
+    // find the length of largest array having ones by swapping k zeros to ones
+       vector<int>num={1,1,1,0,0,0,1,1,1,1,0};
+    int k=2;
+int right =0; int left=0; int length=0; int zero=0;
+while(right<num.size()){
+    if(num[right]==0)  zero++;
+      
+        while(zero>k){
+            if(num[left]==0) zero--;
+            left++;
+        }
+        length=max(length, right-left+1);
+        right++;
+    
+    
+
+}
+cout<<"length is :"<<length;;
+    
+}
